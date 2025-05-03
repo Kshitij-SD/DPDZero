a163efcf6d38170963d517845037a9138282326e
Kshitij-SD <kshitij9802@gmail.com>
2025-05-03 14:31:35 +0530

    data ingestion added

---
+++ src/mlProject/components/data_ingestion.py
@@ -0,0 +1,24 @@
+import os
+import urllib.request as request
+import zipfile
+from mlProject import logger
+from mlProject.utils.common import get_size
+import pandas as pd
+from mlProject.entity.config_entity import DataIngestionConfig
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+    def copy_data(self):
+
+            agents_roster = pd.read_csv(self.config.data_dir + "/agent_roster.csv")
+            call_logs = pd.read_csv(self.config.data_dir + "/call_logs.csv")
+            disposition_summary = pd.read_csv(self.config.data_dir + "/disposition_summary.csv")
+            
+            agents_roster.to_csv(self.config.root_dir + "/agents_roster.csv" ,index=False)
+            call_logs.to_csv(self.config.root_dir + "/call_logs.csv" ,index=False)
+            disposition_summary.to_csv(self.config.root_dir + "/disposition_summary.csv" ,index=False)
+            
+            logger.info("Data Copied to Artifacts")
+            
